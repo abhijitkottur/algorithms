1a0adba323f7e7e9a31131bda225bbcef250b1cd
Abhijit Kottur <abhijit.kottur@foxsports.com.au>
2018-12-24 15:05:47 +1100

    Selection Sort

--- src/main/java/com/abhijit/SelectionSort.java
+++ src/main/java/com/abhijit/SelectionSort.java
@@ -1,4 +1,38 @@
 package com.abhijit;
 
+import java.util.stream.IntStream;
+
 public class SelectionSort {
+
+    public static void main (String[] args) {
+        int[] arr = Utils.buildNewIntArray();
+        int length = arr.length;
+
+        Utils.println("Before sort");
+        IntStream.range(0, length).forEach(i -> Utils.println(arr[i]));
+
+        int[] sortedArr = selectionSort(arr);
+        Utils.println("After sort");
+        IntStream.range(0, length).forEach(i -> Utils.println(sortedArr[i]));
+    }
+
+    private static int[] selectionSort(int[] arr) {
+        int length = arr.length;
+        if (length <= 1) {
+            return arr;
+        }
+
+        int lastUnsortedIndex = length - 1;
+        while(lastUnsortedIndex != 0) {
+            int largestElementIndex = 0;
+            for (int i = 1; i < lastUnsortedIndex; i++) {
+                if (arr[i] > arr[largestElementIndex]) {
+                    largestElementIndex = i;
+                }
+            }
+            Utils.swap(arr, largestElementIndex, lastUnsortedIndex);
+            lastUnsortedIndex--;
+        }
+        return arr;
+    }
 }
